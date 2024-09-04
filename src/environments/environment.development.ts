ebc9b24d2040625f972c2e6c2f4ded321b087a13
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-04 16:10:44 -0500

    chore: define environment variables.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+// Environment: development
+// This is the development environment configuration file.
+// It contains the configuration settings for the development environment.
+// Note: In real scenarios, this file is not submitted to the source repository.
+export const environment = {
+  production: false,
+  newsApiKey: 'fecf4feeffa64e4da682e7d268612ce5'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+// Environment: production
+// This is the production environment configuration file.
+// It contains the configuration settings for the production environment.
+// Note: In real scenarios, this file is not submitted to the source repository.
+export const environment = {
+  production: true,
+  newsApiKey: 'fecf4feeffa64e4da682e7d268612ce5'
+};
