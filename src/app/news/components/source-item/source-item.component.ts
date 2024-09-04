51c7bbee97fe06b50f8ea2826ef059f1ab24ea17
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-04 16:43:04 -0500

    feat(news): add source item component.

---
+++ src/app/news/components/source-item/source-item.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Source} from "../../model/source.entity";
+import {MatListItem, MatListItemAvatar, MatListItemLine} from "@angular/material/list";
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-source-item',
+  standalone: true,
+  imports: [
+    MatListItem,
+    MatListItemAvatar,
+    NgOptimizedImage,
+    MatListItemLine
+  ],
+  templateUrl: './source-item.component.html',
+  styleUrl: './source-item.component.css'
+})
+export class SourceItemComponent {
+  @Input() source!: Source;
+  @Output() sourceSelected = new EventEmitter<Source>();
+
+  onClick() {
+    this.sourceSelected.emit(this.source);
+  }
+}
