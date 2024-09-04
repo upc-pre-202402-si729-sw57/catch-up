493c4ea259b87c307be74536bf851976778196e0
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-04 16:20:20 -0500

    feat(news): add source entity.

---
+++ src/app/news/model/source.entity.ts
@@ -0,0 +1,15 @@
+export class Source {
+  id: string;
+  name: string;
+  url: string;
+  urlToLogo: string;
+
+  constructor() {
+    this.id = "";
+    this.name = "";
+    this.url = "";
+    this.urlToLogo = "";
+  }
+
+
+}
