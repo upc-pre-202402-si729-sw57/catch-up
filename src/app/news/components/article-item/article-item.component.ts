1f7049796c39c65d96f81e481fed0a7edcea93c9
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-07 16:30:34 -0500

    feat(news): add article item component.

---
+++ src/app/news/components/article-item/article-item.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input} from '@angular/core';
+import {Article} from "../../model/article.entity";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardTitleGroup
+} from "@angular/material/card";
+import {MatAnchor, MatIconButton} from "@angular/material/button";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-article-item',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitleGroup,
+    MatCardImage,
+    MatCardContent,
+    MatCardActions,
+    MatAnchor,
+    TranslateModule,
+    MatIconButton,
+    MatIcon
+  ],
+  templateUrl: './article-item.component.html',
+  styleUrl: './article-item.component.css'
+})
+export class ArticleItemComponent {
+  @Input() article!: Article;
+
+}
